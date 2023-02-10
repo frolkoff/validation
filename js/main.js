a9bcf037fd493c0f613b061ed8aa2c4005c38aad
frolkov <markfrolkoff@gmail.com>
2023-02-11 00:30:30 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const regExps = {
+    login: /^[a-z0-9_-]{3,16}$/,
+    email: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+    pass: /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/
+}
+
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('.input');
+
+const inputsData = {};
+
+inputs.forEach( (input) => {
+    inputsData[input.name] = {
+        value: input.value,
+        isValid: false,
+    }
+});
+
+const testValidation = (input) => {
+    if (input.name.includes('confirm')) {
+        const rootInputName = input.name.replace('_confirm', '');
+        // console.log(regExps[rootInputName].test(input.value));
+        // console.log(inputsData[input.name].value === inputsData[rootInputName].value);
+        // console.log(inputsData[input.name])
+        // console.log(input.value)
+        // console.log(inputsData[rootInputName].value)
+        console.log(inputsData)
+        return regExps[rootInputName].test(input.value) && input.value === inputsData[rootInputName].value;
+    }
+    return regExps[input.name].test(input.value)
+}
+
+const changeInput = (input) => {
+    inputsData[input.name] = {
+        value: input.value,
+        isValid: testValidation(input)
+    }
+};
+
+
+form.onchange = (e) => {
+    const target = e.target;
+    if (target.className !== 'input') return;
+    console.log(inputsData)
+    changeInput(target)
+}
+
+// console.log(Object.values(inputsData).map(({isValid}) => isValid).includes(false))
+
+
+
+
