713ac6166e7d503880fa98bd1c5163a8ca65cad2
Mark Frolkov <59063340+frolkoff@users.noreply.github.com>
2023-02-14 11:49:44 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,50 +41,6 @@ form.onchange = (e) => {
 
 // console.log(Object.values(inputsData).map(({isValid}) => isValid).includes(false))
 
-const regExps = {
-    login: /^[a-z0-9_-]{3,16}$/,
-    email: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
-    pass: /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/
-  }
 
-  const inputs = document.querySelectorAll('.input');
-
-  const inputsData = {};
-
-  inputs.forEach( (input) => {
-    input.addEventListener('input', (e) => {
-      changeInput(e.target);
-    })
-    inputsData[input.name] = {
-      value: input.value,
-      isValid: false,
-    }
-  });
-
-  const confirmValidation = (input) => {
-    return regExps[input.dataset['valid']].test(input.value)
-  }
-  const changeInput = (input) => {    
-    inputsData[input.name] = {
-      value: input.value,
-      isValid: confirmValidation(input)
-    }
-    let test = true;
-    let mail = '';
-    for (let i in inputsData) {
-      if (i === 'formValid') continue;
-      if (!inputsData[i].isValid) {
-        test = false;
-        break;
-      }
-      if (!mail.length) {
-        mail = inputsData[i].value;
-        continue;
-      }
-      if (mail !== input.value) test = false
-    }
-    inputsData['formValid'] = test;
-    console.log(inputsData)
-  };
 
 
